7c25971cf149cbb0157568f70ecd5d156fcac5db
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-06 16:26:36 -0500

    working on components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Navigation from "./components/nav/Navigation";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Navigation />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Header.css"
+const header = () => {
+  return (
+    <div>header</div>
+  )
+}
+
+export default header
