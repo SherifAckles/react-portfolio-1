d5292126f7a68063541472d5f407758e00504a1b
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-06 22:19:28 -0500

    work on the header section

---
+++ src/components/header/Cta.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Cv from "../assets/cv.pdf"
+const Cta = () => {
+  return (
+    <div className='cta'>
+        <a href ="{CV} download">Download CV</a>
+        <a href ="#contact">Contact Me</a>
+    </div>
+    
+ 
+  )
+}
+
+export default Cta
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import "./Header.css"
-const header = () => {
+import React from "react";
+import "./header.css";
+import Cta from "./Cta";
+const Header = () => {
   return (
-    <div>header</div>
-  )
-}
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Sherif Moustafa</h1>
+        <h5 className="text-light">Frontend developer</h5>
+        <Cta />
+      </div>
+    </header>
+  );
+};
 
-export default header
+export default Header;
