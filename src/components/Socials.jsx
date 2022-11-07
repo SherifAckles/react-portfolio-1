06a5c7d021778339b8b7072258bbe16dd44984ef
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-07 14:06:44 -0500

    work on social links,use react icons

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Socials = () => {
   return (
-    <div className='Socials'>Socials
-    <a href="http://linkedin.com" target="_blank"></a>
-    <a href="http://linkedin.com" target="_blank"></a>
-    <a href="http://linkedin.com" target="_blank"></a>
+    <div className="Socials">
+      Socials
+      <a href="https://linkedin.com" target="_blank"></a>
+      <a href="https://github.com" target="_blank"></a>
+      <a href="https://fb.com" target="_blank"></a>
     </div>
-  )
-}
+  );
+};
 
-export default Socials
+export default Socials;
