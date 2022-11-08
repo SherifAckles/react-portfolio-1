376a3b8b4d6c1714ef3bcbb51cb51f40230bc16d
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-08 18:16:47 -0500

    import bootstrap

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 import "./experience.css"
 const experience = () => {
   return (
-    <section id='experience'>experience</section>
+    <section id='experience'>
+      <h5>skills</h5>
+      <h2>Experience</h2>
+<div className="container experience__container">
+<div className="experience__frontend">
+  
+</div>
+</div>
+    </section>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./styles.css"
+import "bootstrap/dist/css/bootstrap.css";
+import "./styles.css";
 ReactDOM.render(<App />, document.querySelector("#root"));
