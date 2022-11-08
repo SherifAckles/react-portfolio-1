6d4684f82d364e1b71bcf563bae042c7420438b1
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-07 19:33:52 -0500

    re arange the code

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import "./about.css"
-import Me from "../../assets/me.jpg"
+import React from "react";
+import "./about.css";
+import Me from "../../assets/me.jpg";
 const about = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h4>Get to know</h4>
       <h2>About Me</h2>
-      <div className='container about__container'>
-<div className="about__me">
-<div className="about__me-image">
-<img src={Me} alt="About Image" />
-</div>
-</div>
-<div className="about__content">
-
-</div>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={Me} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content"></div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default about
+export default about;
