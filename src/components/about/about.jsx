01aee5935046e8e65b2ac6c5226d231fcec45b8b
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-08 12:50:06 -0500

    position the about me section

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import Me from "../../assets/me.jpg";
-import {FaAward}from "react-icons/fa"
+import { FaAward } from "react-icons/fa";
 const about = () => {
   return (
     <section id="about">
@@ -16,11 +16,31 @@ const about = () => {
         <div className="about__content">
           <div className="about__cards">
             <article className="about__card">
-              <FaAward className="about__icon"/>
+              <FaAward className="about__icon" />
               <h5>Experience</h5>
-              <small>1+ year</small>
+              <small>1+ year self taught experience</small>
+            </article>
+
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1+ year self taught experience</small>
+            </article>
+
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1+ year self taught experience</small>
             </article>
           </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+              consequatur minus deserunt hic rem ad quis nesciunt, itaque, alias
+              velit magnam maxime aspernatur officiis dolorem similique
+              veritatis dolore dicta eius?
+            </p>
+
+            <a href="#contact"className="btn btn-primary">Contact Me</a>
         </div>
       </div>
     </section>
