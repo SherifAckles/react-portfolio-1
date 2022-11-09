83880c54f4f0a6dbadc078a7e1d5c7dcbb9a3409
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-08 20:01:00 -0500

    add the experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,17 +1,55 @@
-import React from 'react'
-import "./experience.css"
+import React from "react";
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 const experience = () => {
   return (
-    <section id='experience'>
+    <section id="experience">
       <h5>skills</h5>
       <h2>Experience</h2>
-<div className="container experience__container">
-<div className="experience__frontend">
-  
-</div>
-</div>
-    </section>
-  )
-}
+      <div className="experience d-flex justify-content-center">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience-details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </article>
 
-export default experience
+            <article className="experience-details">
+              <BsPatchCheckFill />
+              <h4>CSS</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+
+            <article className="experience-details">
+              <BsPatchCheckFill />
+              <h4>Bootstrap</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+
+            <article className="experience-details">
+              <BsPatchCheckFill />
+              <h4>JavaScript</h4>
+              <small className="text-light">Mid-level</small>
+            </article>
+
+            <article className="experience-details">
+              <BsPatchCheckFill />
+              <h4>JQyery</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+
+            <article className="experience-details">
+              <BsPatchCheckFill />
+              <h4>React JS</h4>
+              <small className="text-light">Mid-level</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default experience;
