de6b4033dc38b6ae28970fecce0b3187cd88a8e3
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-11 20:19:48 -0500

    add test pic 1 and style it

--- src/components/testimonial/testimonial.jsx
+++ src/components/testimonial/testimonial.jsx
@@ -1,35 +1,17 @@
-import React from 'react'
-import "./testimonial.css"
-import Calvin from "../../assets/calvin.png"
-import Jacktest from "../../assets/jacktest.png"
-import Jane from "../../assets/janetest.png"
-import Wanda from "../../assets/wanda.png"
+import React from "react";
+import "./testimonial.css";
+import Calvin from "../../assets/calvin.png";
+import Jacktest from "../../assets/jacktest.png";
+import Jane from "../../assets/janetest.png";
+import Wanda from "../../assets/wanda.png";
 const testimonial = () => {
   return (
-    <section id='testimonial'>
-      <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+    <section id="testimonial">
+     <div>
+            <img src={Calvin} className="calvin" alt="calvin" />
+         </div>
     </section>
-  )
-}
+  );
+};
 
-export default testimonial
+export default testimonial;
