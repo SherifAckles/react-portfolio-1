f9efdb04dc4b4291f6334d38e3aab0bf5c9522ab
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-14 14:55:20 -0500

    start working on the contact me section

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-import "./contact.css"
+import React from "react";
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
 const contact = () => {
   return (
-    <section id='contact'>contact</section>
-  )
-}
+    <section id="contact">
+      <h5>Get in touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container">
+        <div className="contact-option">
+          <article className="contact-option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>sherif.goma89@gmail.com</h5>
+            <a href="mailto:sherif.goma89@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact-option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>sherif.goma89@gmail.com</h5>
+            <a href="mailto:sherif.goma89@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact-option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>sherif.goma89@gmail.com</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=+573138430910"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form action=""></form>
+      </div>
+    </section>
+  );
+};
 
-export default contact
+export default contact;
