b7d6caf9a21a898293111f3116f68557a3b11a21
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-15 16:46:46 -0500

    fix a bug in social links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,25 +11,25 @@ const Footer = () => {
       </a>
       <ul className="footer-links">
         <li>
-          <a href="#"></a>Home
+          <a href="#">Home</a>
         </li>
         <li>
-          <a href="#about"></a>About
+          <a href="#about">About</a>
         </li>
         <li>
-          <a href="#experience"></a>Experience
+          <a href="#experience">Experience</a>
         </li>
         <li>
-          <a href="#services"></a>Services
+          <a href="#services">Services</a>
         </li>
         <li>
-          <a href="#portfolio"></a>Portfolio
+          <a href="#portfolio">Portfolio</a>
         </li>
         <li>
-          <a href="#testimonial"></a>Testimonial
+          <a href="#testimonial">Testimonial</a>
         </li>
         <li>
-          <a href="#contact"></a>Contact
+          <a href="#contact">Contact</a>
         </li>
       </ul>
 
