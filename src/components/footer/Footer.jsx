900e518763d51fdbf5528ada24e440278530a62e
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-15 16:36:24 -0500

    seperate social links from social icon

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,37 +1,54 @@
-import React from 'react'
-import "./footer.css"
-import { AiFillFacebook } from 'react-icons/ai';
-import {AiFillInstagram} from "react-icons/ai"
-import {AiFillTwitterSquare} from "react-icons/ai"
+import React from "react";
+import "./footer.css";
+import { AiFillFacebook } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+import { AiFillTwitterSquare } from "react-icons/ai";
 const Footer = () => {
   return (
-      <footer>
-<a href="#" className='footer-logo'>Ackles</a>
-<ul className='footer-links'>
-  <li><a href="#"></a>Home</li>
-  <li><a href="#about"></a>About</li>
-  <li><a href="#experience"></a>Experience</li>
-  <li><a href="#services"></a>Services</li>
-  <li><a href="#portfolio"></a>Portfolio</li>
-  <li><a href="#testimonial"></a>Testimonial</li>
-  <li><a href="#contact"></a>Contact</li>
+    <footer>
+      <a href="#" className="footer-logo">
+        Ackles
+      </a>
+      <ul className="footer-links">
+        <li>
+          <a href="#"></a>Home
+        </li>
+        <li>
+          <a href="#about"></a>About
+        </li>
+        <li>
+          <a href="#experience"></a>Experience
+        </li>
+        <li>
+          <a href="#services"></a>Services
+        </li>
+        <li>
+          <a href="#portfolio"></a>Portfolio
+        </li>
+        <li>
+          <a href="#testimonial"></a>Testimonial
+        </li>
+        <li>
+          <a href="#contact"></a>Contact
+        </li>
+      </ul>
 
-  <div className="footer-social">
-    <a href="https://facebook.com"><AiFillFacebook /></a>
-    <a href="https://instagram.com"><AiFillInstagram /></a>
-    <a href="https://twitter.com"><AiFillTwitterSquare /></a>
-  </div>
-  <div className="footer-copyright">
-    <small>&copy;Sherif Ackles</small>
-  </div>
-</ul>
+      <div className="footer-social">
+        <a href="https://facebook.com">
+          <AiFillFacebook />
+        </a>
+        <a href="https://instagram.com">
+          <AiFillInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <AiFillTwitterSquare />
+        </a>
+      </div>
+      <div className="footer-copyright">
+        <small>&copy;Sherif Ackles</small>
+      </div>
+    </footer>
+  );
+};
 
-
-
-
-      </footer>
-    
-  )
-}
-
-export default Footer
+export default Footer;
