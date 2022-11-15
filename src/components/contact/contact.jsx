20b2779a773b6d5ea4a1cf1871efc947acc2920d
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-14 21:26:36 -0500

    finished the form with emailjs

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -8,12 +8,13 @@ const contact = () => {
    const form = UseRef(); // useref with u small won't work so we have to import useref as UseRef
   const sendEmail = (e) => {
     e.preventDefault();
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.sendForm('service_llz1c7d', 'template_iws3frs', form.current, 'nEe2vpFMSRKc_xMJi')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
+      e.target.reset()
   };
   return (
     <section id="contact">
