525fdd164281b410d963d7a06568bae39ba2acfd
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-17 20:50:35 -0500

    styling the projects section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,20 +1,172 @@
-import React from 'react'
-import "./projects.css"
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+import "./projects.css";
+import Crm from "../../assets/crm.jpg";
 const Projects = () => {
   return (
-    <section id='projects'>
+    <section id="projects">
       <h5>My recent projects</h5>
       <div className="project-container">
-        <article className='project-item'>
-          <div className="project-image"></div>
-          <h3>Project name</h3>
-          <a href="http://github.com" className='btn' target="_blank" rel="noreferrer">Github</a>
-           <a href="http://github.com" className='btn btn-primary' target="_blank" rel="noreferrer">Live Demo</a> {/*image projects link */}
-
+        <article className="project-item">
+          <div className="project-img">
+            <img src={Crm} />
+          </div>
+          <h3>CRM TOOL</h3>
+          <div className="project-item-cta">
+          <a
+            href="https://github.com/MA5TERMIND2020/CRM-Tool"
+            className="btn"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="http://github.com"
+            className="btn btn-primary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Live Demo
+          </a>{" "}
+          {/*image projects link */}
+          </div>
         </article>
+
+        <article className="project-item">
+          <div className="project-img">
+            <img src={Crm} />
+          </div>
+          <h3>CRM TOOL</h3>
+          <div className="project-item-cta">
+          <a
+            href="https://github.com/MA5TERMIND2020/CRM-Tool"
+            className="btn"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="http://github.com"
+            className="btn btn-primary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Live Demo
+          </a>{" "}
+          {/*image projects link */}
+          </div>
+        </article>
+
+        <article className="project-item">
+          <div className="project-img">
+            <img src={Crm} />
+          </div>
+          <h3>CRM TOOL</h3>
+          <div className="project-item-cta">
+          <a
+            href="https://github.com/MA5TERMIND2020/CRM-Tool"
+            className="btn"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="http://github.com"
+            className="btn btn-primary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Live Demo
+          </a>{" "}
+          {/*image projects link */}
+          </div>
+        </article>
+
+        <article className="project-item">
+          <div className="project-img">
+            <img src={Crm} />
+          </div>
+          <h3>CRM TOOL</h3>
+          <div className="project-item-cta">
+          <a
+            href="https://github.com/MA5TERMIND2020/CRM-Tool"
+            className="btn"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="http://github.com"
+            className="btn btn-primary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Live Demo
+          </a>{" "}
+          {/*image projects link */}
+          </div>
+        </article>
+
+        <article className="project-item">
+          <div className="project-img">
+            <img src={Crm} />
+          </div>
+          <h3>CRM TOOL</h3>
+          <div className="project-item-cta">
+          <a
+            href="https://github.com/MA5TERMIND2020/CRM-Tool"
+            className="btn"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="http://github.com"
+            className="btn btn-primary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Live Demo
+          </a>{" "}
+          {/*image projects link */}
+          </div>
+        </article>
+
+        <article className="project-item">
+          <div className="project-img">
+            <img src={Crm} />
+          </div>
+          <h3>CRM TOOL</h3>
+          <div className="project-item-cta">
+          <a
+            href="https://github.com/MA5TERMIND2020/CRM-Tool"
+            className="btn"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="http://github.com"
+            className="btn btn-primary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Live Demo
+          </a>{" "}
+          {/*image projects link */}
+          </div>
+        </article>
+        
+        
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
