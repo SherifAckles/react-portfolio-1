7aa677378c77ecd744cc18cf9afd755cd5d0500c
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-24 15:23:17 -0500

    trying to fix the netlify bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
-import About from "./components/About/About";
+import About from "./components/About/about";
 import Experience from "./components/experience/Experience";
 import Projects from "./components/projects/Projects";
 import Testimonial from "./components/testimonial/Testimonial";
