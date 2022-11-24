36a422866d0bdbc51df8fce8dfc5f05d8128e162
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-24 15:27:19 -0500

    change file name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
-import About from "./components/About/about";
+import About from "./components/about/about";
 import Experience from "./components/experience/Experience";
 import Projects from "./components/projects/Projects";
 import Testimonial from "./components/testimonial/Testimonial";
