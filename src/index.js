5f3ab23948489787e19ceef2845589f7e9cd287c
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-24 12:53:01 -0500

    test 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./app";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 ReactDOM.render(<App />, document.querySelector("#root"));
