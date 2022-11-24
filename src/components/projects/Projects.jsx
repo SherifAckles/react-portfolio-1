5f74a69a2752e5694b634bee9c30e5ab7633b0a7
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-24 15:49:51 -0500

    change file name

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
-import "./Projects.css";
+import "./projects.css";
 import Crm from "../../assets/crm.jpg";
 const Projects = () => {
   return (
