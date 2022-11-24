7726487539ea8eae32a2712576e311b7ef200337
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-24 18:25:17 -0500

    add testimonials

--- src/components/testimonial/testimonial.jsx
+++ src/components/testimonial/testimonial.jsx
@@ -13,29 +13,43 @@ function Testimonial() {
         <Carousel>
           <Carousel.Item>
             <p className="testimonials">
-              Nulla vitae elit libero, a pharetra augue mollis interdum.
+              Not only was Sherif’s work on-time and to spec, he pays attention
+              to improve the aesthetics of the site as well as the
+              functionality. Throughout the entire process he is responsive, and
+              willing to work through issues as they arise. It’s obvious he
+              takes tremendous pride in his work, and I wouldn’t hesitate to
+              recommend or work with him again.
             </p>
             <img className="avatar" src={Carl} alt="First slide" />
             <p className="from-where">
-              <em>Jacky Milano, New York</em>
+              <em>Carl Milano, New York</em>
             </p>
           </Carousel.Item>
           <Carousel.Item>
             <p className="testimonials">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              We have the fortune of working with Sherif on a very regular basis.
+              Every single project is done in a timely fashion and is delivered
+              exactly as is outlined. Beyond that, his development skills and
+              expertise make him an invaluable part of our process. We
+              absolutely offer our highest possible recommendation.
             </p>
             <img className="avatar" src={Jane} alt="Second slide" />
             <p className="from-where">
-              <em>Jacky Milano, New York</em>
+              <em>Jane Berger, New York</em>
             </p>
           </Carousel.Item>
           <Carousel.Item>
             <p className="testimonials">
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              Working with Sherif was better than expected and we had really
+              high expectations. He is an incredibly talented developer but what
+              really makes him stand out is his work ethic and steady approach.
+              Time after time, and without us asking, he added enhancements and
+              improvements that resulted in a better end product for us and our
+              clients.
             </p>
             <img className="avatar" src={KellyM} alt="Third slide" />
             <p className="from-where">
-              <em>Jacky Milano, New York</em>
+              <em>Kelly M Houston, New York</em>
             </p>
           </Carousel.Item>
         </Carousel>
