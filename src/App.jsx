ceb49d83b1af40d0b2893dc8ddd1e9a5a1e660d1
nabesteron <49988463+nabesteron@users.noreply.github.com>
2022-11-24 12:48:50 -0500

    almost finished fixing netlify bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Header from "./components/header/header";
 import Nav from "./components/nav/nav";
 import About from "./components/about/about";
 import Experience from "./components/experience/experience";
-import Projects from "./components/projects/Projects";
-import Testimonial from "./components/testimonial/Testimonial";
+import Projects from "./components/projects/projects";
+import Testimonial from "./components/testimonial/testimonial";
 import Contact from "./components/contact/contact";
 import Footer from "./components/footer/footer";
 
