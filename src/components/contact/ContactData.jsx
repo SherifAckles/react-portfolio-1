a3a390f8d744218bd4cea973a83b0ff10daeada7
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-05-27 18:45:38 -0500

    created a custom hook to handle sending emails in useEmailSender.js,also contactOptions.js to dynamicly renders the contact.jsx

--- src/components/contact/ContactData.jsx
+++ src/components/contact/ContactData.jsx
@@ -1,74 +1,26 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./contact.css";
-import { MdOutlineEmail } from "react-icons/md";
-import { BsWhatsapp } from "react-icons/bs";
-import { FiSmartphone } from "react-icons/fi";
-import emailjs from "emailjs-com";
+import useEmailSender from "./useEmailSender";
+import { contactOptions } from "./contactOptions";
 
 const ContactData = () => {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-    emailjs
-      .sendForm(
-        "service_llz1c7d",
-        "template_iws3frs",
-        form.current,
-        "nEe2vpFMSRKc_xMJi"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-    e.target.reset();
-  };
+  const { form, sendEmail } = useEmailSender();
 
   return (
     <section id="contact">
       <h2>Contact Me</h2>
       <div className="container contact__container">
         <div className="contact-options">
-          <article className="contact-option">
-            <MdOutlineEmail className="contact-option-icon" />
-            <h4>Email</h4>
-            <h5>sherif.goma89@gmail.com</h5>
-            <a
-              href="mailto:sherif.goma89@gmail.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Send a message
-            </a>
-          </article>
-          <article className="contact-option">
-            <FiSmartphone className="contact-option-icon" />
-            <h4>Phone Number</h4>
-            <h5>+57 313 843 0910</h5>
-            <a
-              href="mailto:sherif.goma89@gmail.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Send a message
-            </a>
-          </article>
-          <article className="contact-option">
-            <BsWhatsapp className="contact-option-icon" />
-            <h4>Whatsapp</h4>
-            <h5>+57 313 843 0910</h5>
-            <a
-              href="https://api.whatsapp.com/send?phone=+573138430910"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Send a message
-            </a>
-          </article>
+          {contactOptions.map(({ icon, title, value, link }, index) => (
+            <article className="contact-option" key={index}>
+              {icon}
+              <h4>{title}</h4>
+              <h5>{value}</h5>
+              <a href={link} target="_blank" rel="noreferrer">
+                Send a message
+              </a>
+            </article>
+          ))}
         </div>
         <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder="Name" required />
