cc338740bebc4efe64cbcc171cbcd2699c31b63d
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-05-27 22:46:50 -0500

    capitalized some components

--- src/components.jsx
+++ src/components.jsx
@@ -3,7 +3,7 @@ import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
 import Projects from "./components/projects/Projects";
-import Testimonial from "./components/testimonial/testimonial";
+import Testimonial from "./components/testimonial/Testimonial";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 
