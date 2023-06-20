18b63d8625ea1ff24441dfc659dcd5ab3a4540cc
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-06-20 08:07:42 -0500

    updated the projects section to use less code and useState hook

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,171 +1,35 @@
-/* eslint-disable jsx-a11y/alt-text */
-import ProjectArray from './projectArray'
-import { Crm, tours, Chatly, Pig, Voz, Fizz } from "./Photos";
+import React, { useState } from "react";
 import "./projects.css";
+import ProjectArray from "./projectArray";
 
 const Projects = () => {
+  const [projects, setProjects] = useState(ProjectArray);
+
   return (
     <section id="projects">
       <h2>My recent projects</h2>
       <div className="project-container">
-        {/* CRM PROJECT */}
-        <article className="project-item">
-          <div className="project-img">
-            <img src={Crm} />
-          </div>
-          <h3>CRM TOOL</h3>
-          <div className="project-item-cta">
-            <a
-              href="https://github.com/MA5TERMIND2020/CRM-Tool"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="http://github.com"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-            {/*image projects link */}
-          </div>
-        </article>
-        {/* TOURS PROJECT */}
-        <article className="project-item">
-          <div className="project-img">
-            <img src={tours} />
-          </div>
-          <h3>Tours</h3>
-          <div className="project-item-cta">
-            <a
-              href="https://github.com/SherifAckles/tours"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              {/* Live Demo */}
-              Coming soon
-            </a>{" "}
-            {/*image projects link */}
-          </div>
-        </article>
-        {/* CHATLY CHAT APP PROJECT */}
-        <article className="project-item">
-          <div className="project-img">
-            <img src={Chatly} />
-          </div>
-          <h3>Chatly 101</h3>
-          <div className="project-item-cta">
-            <a
-              href="https://github.com/nabesteron/chatly"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://chatly101.netlify.app"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-            {/*image projects link */}
-          </div>
-        </article>
-        {/* PIG GAME PROJECT */}
-        <article className="project-item">
-          <div className="project-img">
-            <img src={Pig} />
-          </div>
-          <h3>Pig game</h3>
-          <div className="project-item-cta">
-            <a
-              href="https://github.com/nabesteron/pig-game"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://pig-game-sherif-moustafa.netlify.app/"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-            {/*image projects link */}
-          </div>
-        </article>
-        {/* FIZZ BUZZ APP PROJECT */}
-        <article className="project-item">
-          <div className="project-img">
-            <img src={Fizz} />
-          </div>
-          <h3>Fizz Buzz app</h3>
-          <div className="project-item-cta">
-            <a
-              href="https://github.com/nabesteron/fizz-buzz-app"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://fizz-buzz-app-sherif.netlify.app/"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-            {/*image projects link */}
-          </div>
-        </article>
-
-        {/* LA VOZ DE BOGOTA */}
-        <article className="project-item">
-          <div className="project-img">
-            <img src={Voz} />
-          </div>
-          <h3>La voz de Bogota radio</h3>
-          <div className="project-item-cta">
-            <a
-              href="https://github.com/SherifAckles/la-vos-de-bogota-2"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://la-voz-de-bogota.netlify.app/"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-            {/*image projects link */}
-          </div>
-        </article>
+        {projects.map(({ name, image, github, demo }) => (
+          <article className="project-item" key={name}>
+            <div className="project-img">
+              <img src={image} alt={name} />
+            </div>
+            <h3>{name}</h3>
+            <div className="project-item-cta">
+              <a href={github} className="btn" target="_blank" rel="noreferrer">
+                Github
+              </a>
+              <a
+                href={demo}
+                className={`btn btn-primary ${!demo && "disabled"}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {demo ? "Live Demo" : "Coming soon"}
+              </a>
+            </div>
+          </article>
+        ))}
       </div>
     </section>
   );
--- src/components/projects/projectArray.jsx
+++ src/components/projects/projectArray.jsx
@@ -1,8 +1,41 @@
-const ProjectArray = [
-    {
-        
-    },
+import { Crm, tours, Chatly, Pig, Voz, Fizz } from "./Photos";
 
-    {},];
-
-export default ProjectArray;
+const projectArray = [
+  {
+    name: "CRM TOOL",
+    image: Crm,
+    github: "https://github.com/MA5TERMIND2020/CRM-Tool",
+    demo: "http://github.com",
+  },
+  {
+    name: "Tours",
+    image: tours,
+    github: "https://github.com/SherifAckles/tours",
+    demo: "", // No demo available, set to empty string
+  },
+  {
+    name: "Chatly 101",
+    image: Chatly,
+    github: "https://github.com/nabesteron/chatly",
+    demo: "https://chatly101.netlify.app",
+  },
+  {
+    name: "Pig game",
+    image: Pig,
+    github: "https://github.com/nabesteron/pig-game",
+    demo: "https://pig-game-sherif-moustafa.netlify.app/",
+  },
+  {
+    name: "Fizz Buzz app",
+    image: Fizz,
+    github: "https://github.com/nabesteron/fizz-buzz-app",
+    demo: "https://fizz-buzz-app-sherif.netlify.app/",
+  },
+  {
+    name: "La voz de Bogota radio",
+    image: Voz,
+    github: "https://github.com/SherifAckles/la-vos-de-bogota-2",
+    demo: "https://la-voz-de-bogota.netlify.app/",
+  },
+];
+export default projectArray;
